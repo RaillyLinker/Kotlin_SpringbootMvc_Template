bac0c8b5331936e7494acb21d30492f02f6caf65
prog05 <prog05@ablenara.com>
2024-12-11 17:33:57 +0900

    feat : 스케쥴러 테스트 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ rootProject.name = "kotlin_springboot_mvc_template"
 
 // (모듈 모음)
 // !!!모듈 추가/수정시 아래에 반영!!!
+
+// Spring Cloud 게이트웨이 (8080)
+include("module-cloud-gateway")
+
 // Springboot 프로젝트 모듈 템플릿 (10000)
 include("module-template")
 
@@ -17,14 +21,14 @@ include("module-cloud-eureka2")
 // Spring Cloud 유레카 서버3 (10003)
 include("module-cloud-eureka3")
 
-// Spring Cloud 클라이언트 샘플 (12000)
-include("module-sample-cloud-client")
-
-// Spring Cloud 클라이언트 샘플 복제 (12001)
-include("module-sample-cloud-client-copy")
-
-// Spring Cloud 게이트웨이 (8080)
-include("module-cloud-gateway")
-
 // 인증/인가 서버 (11000)
 include("module-auth")
+
+// Spring Cloud 클라이언트 샘플 (12001)
+include("module-sample-cloud-client")
+
+// Spring Cloud 클라이언트 샘플 복제 (12002)
+include("module-sample-cloud-client-copy")
+
+// 스케쥴러 테스트 샘플 (12003)
+include("module-sample-scheduler")
