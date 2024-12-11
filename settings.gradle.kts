01258f0cbbc8e22b404a6842dded16be88ab0632
prog05 <prog05@ablenara.com>
2024-12-11 11:19:26 +0900

    feat : 멀티 모듈 구조 추가 및 template 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "kotlin_springboot_mvc_template"
+
+// (모듈 모음)
+// !!!모듈 추가/수정시 아래에 반영!!!
+include("module-template")
