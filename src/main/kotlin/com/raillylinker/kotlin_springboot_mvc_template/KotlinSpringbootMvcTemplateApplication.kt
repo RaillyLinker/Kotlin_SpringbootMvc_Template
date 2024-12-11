4203a0c0172d8eb376ecb734a6851d2dc8755fa4
prog05 <prog05@ablenara.com>
2024-12-11 10:46:09 +0900

    feat : springboot 프로젝트 생성

---
+++ src/main/kotlin/com/raillylinker/kotlin_springboot_mvc_template/KotlinSpringbootMvcTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.raillylinker.kotlin_springboot_mvc_template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootMvcTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootMvcTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/raillylinker/kotlin_springboot_mvc_template/KotlinSpringbootMvcTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raillylinker.kotlin_springboot_mvc_template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootMvcTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
