c88f344446bb27df7049f4a40df6d6812ad5c878
prog05 <prog05@ablenara.com>
2024-12-16 17:54:04 +0900

    feat : 게시글 DB 테이블 Entity 추가

--- module-portfolio-board/src/main/kotlin/com/raillylinker/module_portfolio_board/jpa_beans/db1_main/entities/Db1_RaillyLinkerCompany_TotalAuthMember.kt
+++ module-portfolio-board/src/main/kotlin/com/raillylinker/module_portfolio_board/jpa_beans/db1_main/entities/Db1_RaillyLinkerCompany_TotalAuthMember.kt
@@ -73,6 +73,12 @@ class Db1_RaillyLinkerCompany_TotalAuthMember(
     @OneToMany(mappedBy = "totalAuthMember", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     var totalAuthMemberEmailList: MutableList<Db1_RaillyLinkerCompany_TotalAuthMemberEmail> = mutableListOf()
 
+    @OneToMany(mappedBy = "totalAuthMember", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var sampleBoardList: MutableList<Db1_RaillyLinkerCompany_SampleBoard> = mutableListOf()
+
+    @OneToMany(mappedBy = "totalAuthMember", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var sampleBoardCommentList: MutableList<Db1_RaillyLinkerCompany_SampleBoardComment> = mutableListOf()
+
 
     // ---------------------------------------------------------------------------------------------
     // <중첩 클래스 공간>
