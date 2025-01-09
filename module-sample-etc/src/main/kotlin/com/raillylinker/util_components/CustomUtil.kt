2dfebff7cf3e5100273b8cb781af54ef1a2e7bbb
prog05 <prog05@ablenara.com>
2025-01-09 10:08:13 +0900

    feat : 파일 경로 분리 함수 및 테스트 코드 추가

--- module-sample-etc/src/main/kotlin/com/raillylinker/util_components/CustomUtil.kt
+++ module-sample-etc/src/main/kotlin/com/raillylinker/util_components/CustomUtil.kt
@@ -124,6 +124,23 @@ class CustomUtil {
         return rad * 180 / Math.PI
     }
 
+    // (파일명, 경로, 확장자 분리 함수)
+    // sample.jpg -> sample, jpg
+    fun splitFilePath(filePath: String): FilePathParts {
+        val fileName = filePath.substringBeforeLast(".", filePath) // 확장자가 없다면 전체 파일 이름이 그대로 fileName
+        val extension = if (fileName != filePath) filePath.substringAfterLast(".", "") else null
+
+        return FilePathParts(
+            fileName = fileName,
+            extension = extension
+        )
+    }
+
+    data class FilePathParts(
+        val fileName: String,
+        val extension: String?
+    )
+
 
     // ---------------------------------------------------------------------------------------------
     // <중첩 클래스 공간>
--- module-sample-etc/src/test/kotlin/com/raillylinker/ModuleTest.kt
+++ module-sample-etc/src/test/kotlin/com/raillylinker/ModuleTest.kt
@@ -1,16 +1,29 @@
 package com.raillylinker
 
+import com.raillylinker.util_components.CustomUtil
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
 
 @SpringBootTest
 class ModuleTest {
     private val classLogger: Logger = LoggerFactory.getLogger(this::class.java)
 
+    @Autowired
+    private lateinit var customUtil: CustomUtil
+
+    // (파일명, 경로, 확장자 분리 함수 테스트)
     @Test
-    fun test() {
-        classLogger.info("test")
+    fun splitFilePathTest() {
+        val result = customUtil.splitFilePath("sample.jpg")
+        assertEquals("sample", result.fileName)
+        assertEquals("jpg", result.extension)
+
+        val result2 = customUtil.splitFilePath("sample")
+        assertEquals("sample", result2.fileName)
+        assertEquals(null, result2.extension)
     }
 }
