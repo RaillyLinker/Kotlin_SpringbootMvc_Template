3f486583f4fea324399d7f4286358760dc2d6612
RaillyLinker <raillylinker@gmail.com>
2025-01-24 10:26:00 +0900

    docs : Spring Cloud 관련 설명 주석 추가

--- module-cloud-gateway/src/main/kotlin/com/raillylinker/ApplicationMain.kt
+++ module-cloud-gateway/src/main/kotlin/com/raillylinker/ApplicationMain.kt
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import java.util.*
 
+// Spring Cloud Eureka 에서 등록된 마이크로 서비스의 api 가 503(Service Unavailable) 을 반환하면,
+// 동일하게 등록된 다른 복제 서버에 로드 밸런싱을 하게 됩니다.
 @ComponentScan(
     // !!!Bean 스캔할 모듈들의 패키지 리스트(group) 추가하기!!!
     basePackages =
